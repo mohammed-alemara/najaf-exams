812764d06498e3549ea6024dfc3c0d69b2ee88f2
Mohammed Al-Emara <mmh.mart@gmail.com>
2026-01-13 02:53:33 +0300

    update

---
+++ home.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    setTimeout(() => {
+        document.querySelector(".loading-screen").style.display = "none";
+        document.querySelector(".container").style.display = "block";
+    }, 1500);
+};
