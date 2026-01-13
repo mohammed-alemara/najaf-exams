296eef62bc9fc1f64ba5ad125ba8a51cb8fec602
Mohammed Al-Emara <mmh.mart@gmail.com>
2026-01-13 03:11:06 +0300

    update

--- js/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    setTimeout(function () {
-        document.querySelector(".loading-screen").style.display = "none";
-        document.querySelector(".container").style.display = "block";
-    }, 1200);
-};
