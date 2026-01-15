77f12a3af48fd344ba1e37222b044595b230b392
Mohammed Al-Emara <mmh.mart@gmail.com>
2026-01-15 12:05:23 +0300

    update

---
+++ js/t1.js
@@ -0,0 +1,7 @@
+Cognito.overrideText("fileupload-dropzone-message", "");
+Cognito.overrideText("fileupload-default-upload-button-text", "رفع الصورة الشخصية");
+Cognito.overrideText("fileupload-upload-inprogress-message", "جاري رفع الملف");
+Cognito.overrideText("fileupload-excluded-file-type-message", "نوع الملف غير مدعوم");
+Cognito.overrideText("fileupload-invalid-file-type-message", "يجب ان يكون نوع الملف {allowedTypes}");
+Cognito.overrideText("required", "حقل {property} مطلوب");
+Cognito.overrideText("string-length-between", "المدخل غير صحيح");
